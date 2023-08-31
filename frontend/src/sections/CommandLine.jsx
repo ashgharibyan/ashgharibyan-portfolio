b94eef6ab02cc7ae98f373c7b136c424d0df6ad9
Ashot Gharibyan <119915091+ashgharibyan@users.noreply.github.com>
2023-08-30 20:25:23 -0700

    input button added

--- frontend/src/sections/CommandLine.jsx
+++ frontend/src/sections/CommandLine.jsx
@@ -2,8 +2,12 @@ import CodeLine from "../components/CodeLine";
 
 const CommandLine = () => {
     return (
-        <div>
-            <CodeLine text="(ashot_gharibyan_env) hello@Macbook-Pro ~ % "></CodeLine>
+        <div className="flex items-center justify-start gap-4">
+            <CodeLine
+                text="(ashot_gharibyan_env) hello@Macbook-Pro ~ % "
+                extraStyles="flex-none"
+            ></CodeLine>
+            <input type="text" className="textQuery grow text-[#1e1e1e]" />
         </div>
     );
 };
