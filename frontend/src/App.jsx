d6b66c41028690a4538d2b1893c6664f0a0aa457
ashgharibyan <ashghcode@gmail.com>
2023-09-06 01:48:07 -0700

    fixed height issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
                 <section className="bg-[#3F3836] px-8 py-4 text-3xl text-[#BAB3B2]">
                     <Header />
                 </section>
-                <section className="h-fit bg-[#1e1e1e] px-8 py-10 pt-4 text-3xl text-white">
+                <section className="max-h-fit min-h-screen bg-[#1e1e1e] px-8 py-10 pt-4 text-3xl text-white">
                     <section className="">
                         <Demo />
                     </section>
