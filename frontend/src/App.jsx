0dd258799bfc93d2839a92f415cdd0300eab67d1
ashgharibyan <ashghcode@gmail.com>
2023-09-06 01:44:28 -0700

    About done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
                 <section className="bg-[#3F3836] px-8 py-4 text-3xl text-[#BAB3B2]">
                     <Header />
                 </section>
-                <section className="h-screen bg-[#1e1e1e] px-8 pt-4 text-3xl text-white">
+                <section className="h-fit bg-[#1e1e1e] px-8 py-10 pt-4 text-3xl text-white">
                     <section className="">
                         <Demo />
                     </section>
--- frontend/src/results/About.jsx
+++ frontend/src/results/About.jsx
@@ -1,32 +1,51 @@
+import CodeLine from "../components/CodeLine";
+
 const About = () => {
     return (
         <div className="textQuery">
-            <p>Name: Ashot Gharibyan </p>
-            <p></p>
-            <p>Full Stack Developer based in Los Angeles, CA</p>
-            <p></p>
-            <p>
-                I am a highly motivated and skilled Full Stack Developer with a
-                passion for creating beautiful, functional and user-friendly
-                websites. With a strong background in Python, Java, JavaScript,
-                HTML5, CSS and various web development frameworks and libraries,
-                I have the technical skills and experience to turn your ideas
-                into reality. I have worked on a wide range of projects, from
-                simple websites to complex web applications and I am always
-                eager to learn new technologies and development methods.
-            </p>
-            <p></p>
-            <p>
-                I am also a self-starter and a fast learner, with a keen eye for
-                detail and a commitment to producing high-quality work. Whether
-                working independently or as part of a team, I am always focused
-                on delivering projects that meet or exceed expectations.
-            </p>
-            <p></p>
-            <p>
-                Feel free to contact me to discuss how I can bring my skills and
-                experience to your next project.
-            </p>
+            <CodeLine text={""} />
+            <CodeLine text={""} />
+            <CodeLine
+                text={"------------------------------------------------"}
+                extraStyles={"text-[#F4BD4F]"}
+            />
+            <CodeLine
+                text={"Name: Ashot Gharibyan"}
+                extraStyles={"text-[#F4BD4F]"}
+            />
+            <CodeLine
+                text={"Full Stack Developer based in Los Angeles, CA"}
+                extraStyles={"text-[#F4BD4F]"}
+            />
+            <CodeLine
+                text={"------------------------------------------------"}
+                extraStyles={"text-[#F4BD4F]"}
+            />
+            <CodeLine
+                text={
+                    "I am a highly motivated and skilled Full Stack Developer with a passion for creating beautiful, functional and user-friendly websites. With a strong background in Python, Java, JavaScript, HTML5, CSS and various web development frameworks and libraries, I have the technical skills and experience to turn your ideas into reality. I have worked on a wide range of projects, from simple websites to complex web applications and I am always eager to learn new technologies and development methods."
+                }
+                extraStyles={"text-[#]"}
+            />
+
+            <CodeLine
+                text={"------------------------------------------------"}
+            />
+            <CodeLine
+                text={
+                    "I have keen eye for detail and a commitment to producing high-quality work. Whether working independently or as part of a team, I am always focused on delivering projects that meet or exceed expectations."
+                }
+                extraStyles={"text-[#]"}
+            />
+            <CodeLine
+                text={"------------------------------------------------"}
+            />
+            <CodeLine
+                text={
+                    "Feel free to contact me to discuss how I can bring my skills and experience to your next project."
+                }
+                extraStyles={"text-[#61C454]"}
+            />
         </div>
     );
 };
