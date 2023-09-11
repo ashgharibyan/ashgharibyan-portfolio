65f5ecb1d0e696b24611aa4fc731709c6367a6d9
ashgharibyan <ashghcode@gmail.com>
2023-09-11 15:56:20 -0700

    creating results

---
+++ frontend/src/results/index.js
@@ -0,0 +1,8 @@
+import About from "./About";
+import Contact from "./Contact";
+import Education from "./Education";
+import Experience from "./Experience";
+import Projects from "./Projects";
+import Stacks from "./Stacks";
+
+export { About, Contact, Education, Experience, Projects, Stacks };
