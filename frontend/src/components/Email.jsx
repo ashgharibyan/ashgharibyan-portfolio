aae281b81904bc2bd9c476e06601352be4c7e825
ashgharibyan <ashghcode@gmail.com>
2023-09-13 02:45:14 -0700

    made all the form fields, need to fix the cursor movement

--- frontend/src/components/Email.jsx
+++ frontend/src/components/Email.jsx
@@ -7,8 +7,12 @@ const Email = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
-    const { nameRef } = useContext(InputContext);
+    const { nameRef, emailRef, messageRef, sendRef } = useContext(InputContext);
     const [inputError, setInputError] = useState("");
+    const [showEmail, setShowEmail] = useState(false);
+    const [showMessage, setShowMessage] = useState(false);
+    const [sendMessage, setSendMessage] = useState(false);
+    const [sendYN, setSendYN] = useState("");
 
     function sendEmail(e) {}
 
@@ -17,9 +21,16 @@ const Email = () => {
         nameRef.current.focus();
     }, []);
 
-    const handleKeyDown = (e) => {
+    const handleNameKeyDown = (e) => {
         if (e.key === "Enter") {
             e.preventDefault(); // Prevents a new line from being added
+            if (name === "") {
+                setInputError("Please enter your name!");
+                nameRef.current.focus();
+            } else {
+                setShowEmail(true);
+                emailRef.current.focus();
+            }
         }
     };
 
@@ -38,6 +49,104 @@ const Email = () => {
         }
     };
 
+    const handleEmailKeyDown = (e) => {
+        if (e.key === "Enter") {
+            e.preventDefault(); // Prevents a new line from being added
+            if (email === "") {
+                setInputError("Please enter your email!");
+                emailRef.current.focus();
+            } else {
+                setShowMessage(true);
+                messageRef.current.focus();
+            }
+        }
+    };
+
+    const handleEmailChange = (e) => {
+        const maxLen = 30;
+        const currentLength = e.target.value.length;
+
+        if (currentLength > maxLen) {
+            e.target.value = e.target.value.substring(0, maxLen);
+            setInputError(
+                "Maximum length of the email is 30 characters. Please Try Again!",
+            );
+        } else {
+            setEmail(e.target.value);
+            setInputError("");
+        }
+    };
+
+    const handleMessageKeyDown = (e) => {
+        if (e.key === "Enter") {
+            e.preventDefault(); // Prevents a new line from being added
+            if (message === "") {
+                setInputError("Please enter your message!");
+                messageRef.current.focus();
+            } else {
+                setSendMessage(true);
+                sendRef.current.focus();
+            }
+        }
+    };
+
+    const handleMessageChange = (e) => {
+        const maxLen = 30;
+        const currentLength = e.target.value.length;
+
+        if (currentLength > maxLen) {
+            e.target.value = e.target.value.substring(0, maxLen);
+            setInputError(
+                "Maximum length of the message is 30 characters. Please Try Again!",
+            );
+        } else {
+            setMessage(e.target.value);
+            setInputError("");
+        }
+    };
+
+    const handleSendKeyDown = (e) => {
+        if (e.key === "Enter") {
+            e.preventDefault(); // Prevents a new line from being added
+            if (sendYN === "") {
+                setInputError(
+                    "Field cannot be empty! Please enter 'y' or 'n'!",
+                );
+                sendRef.current.focus();
+            } else if (sendYN === "n") {
+                setInputError("Message not sent!");
+            } else if (sendYN === "y") {
+                setInputError("Message sent!");
+            } else {
+                setInputError("Please enter 'y' or 'n' to send the message!");
+                sendRef.current.focus();
+            }
+        }
+    };
+
+    const handleSendChange = (e) => {
+        const maxLen = 1;
+        const currentLength = e.target.value.length;
+
+        if (currentLength > maxLen) {
+            e.target.value = e.target.value.substring(0, maxLen);
+            setInputError(
+                "Please enter 'y' or 'n' to send the message. Please Try Again!",
+            );
+        } else if (
+            e.target.value != "y" &&
+            e.target.value != "n" &&
+            e.target.value != ""
+        ) {
+            setInputError(
+                "Please enter 'y' or 'n' to send the message. Please Try Again!",
+            );
+        } else {
+            setSendYN(e.target.value);
+            setInputError("");
+        }
+    };
+
     return (
         <div>
             <CodeLine text={""} />
@@ -47,11 +156,58 @@ const Email = () => {
                     ref={nameRef}
                     type="text"
                     className="textQuery min-w-0 flex-grow whitespace-pre-wrap  break-words rounded border-none bg-[#1e1e1e] p-2 text-[white] outline-none"
-                    onKeyDown={handleKeyDown}
+                    onKeyDown={handleNameKeyDown}
                     value={name}
                     onChange={handleNameChange}
+                    readOnly={showEmail ? true : false}
                 />
             </div>
+
+            {showEmail && (
+                <div className="textQuery flex items-center gap-4">
+                    <span className="flex-none">Please enter your email:</span>
+                    <input
+                        ref={emailRef}
+                        type="text"
+                        className="textQuery min-w-0 flex-grow whitespace-pre-wrap  break-words rounded border-none bg-[#1e1e1e] p-2 text-[white] outline-none"
+                        onKeyDown={handleEmailKeyDown}
+                        value={email}
+                        onChange={handleEmailChange}
+                        readOnly={showMessage ? true : false}
+                    />
+                </div>
+            )}
+
+            {showMessage && (
+                <div className="textQuery flex items-center gap-4">
+                    <span className="flex-none">
+                        Please enter your message:
+                    </span>
+                    <input
+                        ref={messageRef}
+                        type="text"
+                        className="textQuery min-w-0 flex-grow whitespace-pre-wrap  break-words rounded border-none bg-[#1e1e1e] p-2 text-[white] outline-none"
+                        onKeyDown={handleMessageKeyDown}
+                        value={message}
+                        onChange={handleMessageChange}
+                        readOnly={sendMessage ? true : false}
+                    />
+                </div>
+            )}
+
+            {sendMessage && (
+                <div className="textQuery flex items-center gap-4">
+                    <span className="flex-none">Send the message? (y/n):</span>
+                    <input
+                        ref={sendRef}
+                        type="text"
+                        className="textQuery min-w-0 flex-grow whitespace-pre-wrap  break-words rounded border-none bg-[#1e1e1e] p-2 text-[white] outline-none"
+                        onKeyDown={handleSendKeyDown}
+                        value={sendYN}
+                        onChange={handleSendChange}
+                    />
+                </div>
+            )}
 
             <CodeLine text={inputError} extraStyles={"text-red-500"} />
         </div>
--- frontend/src/contexts/InputContext.jsx
+++ frontend/src/contexts/InputContext.jsx
@@ -6,10 +6,22 @@ export const InputProvider = ({ children }) => {
     const [input, setInput] = useState("");
     const [command, setCommand] = useState("");
     const nameRef = useRef(null);
+    const emailRef = useRef(null);
+    const messageRef = useRef(null);
+    const sendRef = useRef(null);
 
     return (
         <InputContext.Provider
-            value={{ input, setInput, command, setCommand, nameRef }}
+            value={{
+                input,
+                setInput,
+                command,
+                setCommand,
+                nameRef,
+                emailRef,
+                messageRef,
+                sendRef,
+            }}
         >
             {children}
         </InputContext.Provider>
