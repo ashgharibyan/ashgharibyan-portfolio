5513ff8c012e66783e3b78b73c82ba3c2380c1ed
ashgharibyan <ashghcode@gmail.com>
2023-09-18 15:11:35 -0700

    good version

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,22 +10,26 @@ import {
 const App = () => {
     return (
         <InputProvider>
-            <main>
-                <section className="bg-[#3F3836] px-8 py-4 text-3xl text-[#BAB3B2]">
-                    <Header />
-                </section>
-                <section className="max-h-fit min-h-screen bg-[#1e1e1e] px-8 py-10 pt-4 text-3xl text-white">
-                    <section className="">
-                        <Demo />
+            <div className="bg-[#1e1e1e]">
+                <main>
+                    <section className="bg-[#3F3836] px-8 py-4 text-3xl text-[#BAB3B2]">
+                        <Header />
                     </section>
-                    <section className="">
-                        <CommandLine />
+                    <section className="max-h-fit min-h-screen bg-[#1e1e1e] px-8 py-10 pt-4 text-3xl text-white">
+                        <section className="">
+                            <Demo />
+                        </section>
+                        <section className="">
+                            <CommandLine />
+                        </section>
                     </section>
-                </section>
-                <section className="px-8">
+                </main>
+
+                {/* TODO 
+                <section className="rounded-lg backdrop-blur-lg">
                     <Footer />
-                </section>
-            </main>
+                </section> */}
+            </div>
         </InputProvider>
     );
 };
--- frontend/src/sections/Footer.jsx
+++ frontend/src/sections/Footer.jsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <footer className="flex items-center justify-center gap-2 p-2">
+            TODO
+        </footer>
+    );
 };
 
 export default Footer;
