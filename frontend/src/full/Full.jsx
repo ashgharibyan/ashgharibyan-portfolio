9f05230288cd36fdc28e6ca6a210c6ce61c3da8a
ashgharibyan <>
2023-09-22 04:04:09 -0700

    fix: full scroll to top

--- frontend/src/full/Full.jsx
+++ frontend/src/full/Full.jsx
@@ -1,8 +1,15 @@
+import { useLocation } from "react-router-dom";
 import { FullProvider } from "../contexts/FullContext";
 import FullContent from "./FullContent";
 import TerminalButton from "./components/TerminalButton";
+import { useEffect } from "react";
 
 const Full = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [location.pathname]);
     return (
         <FullProvider>
             <TerminalButton />
